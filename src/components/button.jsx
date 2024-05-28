74494044c3e02ecd8c712407704ae4884f34e821
Leonardo Rico <104222321+LeonardoRico@users.noreply.github.com>
2024-05-28 00:48:07 -0500

    Add files via upload

---
+++ src/components/button.jsx
@@ -0,0 +1,12 @@
+//funcion tipo flecha 
+const Button = (props) => {
+
+    const { name } = props;
+    return (
+        <>
+            <button className="bg-primary px-5 py-2 text-white">{name}</button>
+        </>
+    )
+}
+
+export default Button;
